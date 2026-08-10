745d7daf83f8ab33fb50cd65fc4d0cd5e0889461
ashley-rai <assylayraimova@riversideschool.cz>
2026-08-10 15:35:15 +0200

    reformated hw

---
+++ mobydev/hw 10.py
@@ -0,0 +1,53 @@
+from functions import printNum
+
+printNum(1)
+
+
+def introduceMyself(name: str, age: int):
+    print("Hello, my name is " + name + " and I am " + str(age))
+
+
+introduceMyself("Assylay", age=12)
+
+printNum(2)
+
+
+def sayHello(name: str):
+    print(f"Hello, {name}")
+
+
+sayHello("Vladislav")
+sayHello("Amine")
+sayHello("Daniil")
+
+printNum(3)
+
+
+def introduction(name: str, home: str, age: int):
+    print(f"{name}, {age} года, город {home}")
+
+
+introduction("Олжас", "Алматы", 34)
+
+printNum(4)
+
+
+def progressUpdate(steps: int, goal: int):
+    percentOfGoal = steps / goal * 100
+    print(percentOfGoal)
+
+    if percentOfGoal < 10:
+        print("У вас хорошее начало")
+    elif percentOfGoal < 50:
+        print("Вы почти на полпути!")
+    elif percentOfGoal < 90:
+        print("Вы на полпути!")
+    elif percentOfGoal < 100:
+        print("Вы почти у цели!")
+    else:
+        print("Вы превзошли свою цель!")
+
+
+progressUpdate(2, 2)
+progressUpdate(6, 221)
+progressUpdate(25, 52)
